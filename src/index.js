4d1694fb5e14e030ddd4d90017f4693faa73cf3c
Carlos <carlosforjadores@gmail.com>
2024-02-08 22:09:04 -0800

    initial auth0 integration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const { auth } = require("express-openid-connect");
+const { requiresAuth } = require("express-openid-connect");
 
 const usersRouter = require("./api/users");
+
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3500;
@@ -9,12 +13,34 @@ app.use(express.json());
 
 const router = express.Router();
 
-router.use("/users", usersRouter);
+//oauth
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH_SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+router.use("/users", requiresAuth(), usersRouter);
 
 app.use("/api/", router);
 
 const server = app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}/api/`);
+  console.log(`Server is running on http://localhost:${PORT}/`);
 });
 
 module.exports = server;
